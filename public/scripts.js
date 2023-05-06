2a69c35fdba9f8126483cf238319710ccd16b4a7
salfra <salfra@email.unc.edu>
2023-05-06 16:38:59 -0400

    Added showHideShots function

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,18 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+
+function showHideShots(){
+	let check = document.getElementById('opponent');
+	let type = document.querySelector('input[name="game"]:checked').id;
+	if(check.checked && type === 'rpsls'){
+		$('.shots').show();
+		$('.rpsls').show();
+	} else if(check.checked && type === 'rps'){
+		$('.shots.rps').show();
+		$('.rpsls').hide();
+	} else {
+		$('.shots').hide();
+	}
+}
